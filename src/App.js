e4a20609d8577cd83f64e044c1b475c9998359e6
Artem Minin <timeap@yandex.ru>
2022-11-22 06:35:25 +0300

    added card props

--- src/App.js
+++ src/App.js
@@ -10,11 +10,13 @@ function App() {
       <MyComponent />
       <MyClassComponent />
       <div className='card_container'>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card image='https://fikiwiki.com/uploads/posts/2022-02/1644968674_1-fikiwiki-com-p-krasivie-kartinki-4k-na-telefon-1.jpg' title= 'Заголовок1' description='Гора — форма рельефа, изолированное резкое поднятие местности.' price='100'/>
+        <Card image='https://fikiwiki.com/uploads/posts/2022-02/1644968674_1-fikiwiki-com-p-krasivie-kartinki-4k-na-telefon-1.jpg' title= 'Заголовок2' description='Гора — форма рельефа, изолированное резкое поднятие местности.' price='200'/>
+        <Card image='https://fikiwiki.com/uploads/posts/2022-02/1644968674_1-fikiwiki-com-p-krasivie-kartinki-4k-na-telefon-1.jpg' title= 'Заголовок3' description='Гора — форма рельефа, изолированное резкое поднятие местности.' price='300'/>
+        <Card image='https://fikiwiki.com/uploads/posts/2022-02/1644968674_1-fikiwiki-com-p-krasivie-kartinki-4k-na-telefon-1.jpg' title= 'Заголовок4' description='Гора — форма рельефа, изолированное резкое поднятие местности.' price='400'/>
+        <Card image='https://fikiwiki.com/uploads/posts/2022-02/1644968674_1-fikiwiki-com-p-krasivie-kartinki-4k-na-telefon-1.jpg' title= 'Заголовок5' description='Гора — форма рельефа, изолированное резкое поднятие местности.' price='500'/>
+        <Card image='https://fikiwiki.com/uploads/posts/2022-02/1644968674_1-fikiwiki-com-p-krasivie-kartinki-4k-na-telefon-1.jpg' title= 'Заголовок6' description='Гора — форма рельефа, изолированное резкое поднятие местности.' price='600'/>
+        <Card image='https://fikiwiki.com/uploads/posts/2022-02/1644968674_1-fikiwiki-com-p-krasivie-kartinki-4k-na-telefon-1.jpg' title= 'Заголовок7' description='Гора — форма рельефа, изолированное резкое поднятие местности.' price='700'/>
       </div>
       <div className='App-newsletter'>
         <h2>Подписаться на новости компании</h2>
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,14 @@
 import './Card.css';
 import Button from '../Button/Button';
 
-function Card() {
+function Card(props) {
   return(
     <div className='card'>
-      <img className='card__img' src='https://fikiwiki.com/uploads/posts/2022-02/1644968674_1-fikiwiki-com-p-krasivie-kartinki-4k-na-telefon-1.jpg' alt='logo' />
+      <img className='card__img' src={ props.image } alt='logo' />
       <div className='card__container'>
-        <h2 className='card__title'>Заголовок</h2>
-        <p className='card__description'>Гора — форма рельефа, изолированное резкое поднятие местности.</p>
-        <div className='card__price common__price'>300</div>
+        <h2 className='card__title'>{ props.title }</h2>
+        <p className='card__description'>{ props.description }</p>
+        <div className='card__price common__price'>{ props.price }</div>
         <Button text='Купить'/>
       </div>
     </div>
